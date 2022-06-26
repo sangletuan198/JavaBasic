aa93930091ec29f0c5a35f7ae47d6f09bf97350a
sangletuan198 <sangletuan198@gmail.com>
2022-06-26 17:09:11 +0700

    create new project for java basic

---
+++ src/comon/Main.java
@@ -0,0 +1,18 @@
+package comon;
+
+import day1.DataTypesAndVariables;
+
+
+public class Main {
+	public static void main(String[] args) {
+		//ex1
+		DataTypesAndVariables.printMessage("\n" + "*******bai 1******" + "\n");
+		DataTypesAndVariables.printMessage("Hello Java");
+		//ex2
+		DataTypesAndVariables.printMessage("\n" + "******bai2******" + "\n");
+		DataTypesAndVariables.printMessage("++++++" + "\n\n" + "@@@@@@" +"\n\n" + "******" + "\n\n" + "######");
+		//ex3
+		DataTypesAndVariables.printMessage("\n" + "******bai3******" + "\n");
+		DataTypesAndVariables.printVariable();
+	}
+}
---
+++ src/day1/DataTypesAndVariables.java
@@ -0,0 +1,16 @@
+package day1;
+
+public class DataTypesAndVariables { 
+	public static void printMessage(String message) {
+	     System.out.print(message);  
+	    }  
+	public static void printVariable() {
+		byte a =10;
+		float b = 20.3f;
+		float c = 3.14785f;
+		System.out.println("print a: " + a);
+		System.out.println("print b: " + b);
+		System.out.println("print c: " + c);
+}
+}
+
